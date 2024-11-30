5f2a6c6a966f982756c41712c48a4acd5b5bf6c3
PlanetSat0rn <109532054+PlanetSat0rn@users.noreply.github.com>
2024-11-30 16:35:44 -0500

    Initial commit

---
+++ src/main/java/planetsaturn/industry/SaturnIndustryDataGenerator.java
@@ -0,0 +1,11 @@
+package planetsaturn.industry;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SaturnIndustryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
